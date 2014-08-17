248047cbd314fc96731b431dcf1d19ee133bc63a
gjgfuj <gjgfujb@gmail.com>
2014-08-17 11:05:23 +1000

    Added fluid stuff.

--- main/scala/com/awesomesauce/minecraft/forge/awesometech/api/TMachine.scala
+++ main/scala/com/awesomesauce/minecraft/forge/awesometech/api/TMachine.scala
@@ -1,14 +1,16 @@
 package com.awesomesauce.minecraft.forge.awesometech.api
 
 import net.minecraft.item.ItemStack
-import net.minecraftforge.fluids.{FluidStack, Fluid}
+import net.minecraftforge.fluids.FluidStack
 
 trait TMachine {
   def drainRF(amount: Int, simulate: Boolean) : Int
-  def drainFluid(fluid: FluidStack, simulate:Boolean) : Boolean
+
+  def drainFluid(fluid: FluidStack, simulate: Boolean): FluidStack
   def drainItem(item: ItemStack, simulate:Boolean) : Boolean
   def insertRF(amount: Int, simulate: Boolean) : Int
-  def insertFluid(fluid:FluidStack, simulate: Boolean) : Boolean
+
+  def insertFluid(fluid: FluidStack, simulate: Boolean): FluidStack
   def insertItem(item: ItemStack, simulate: Boolean) : Boolean
   def getX: Int
   def getY: Int
--- main/scala/com/awesomesauce/minecraft/forge/awesometech/tile/machine/TileEntityMachineController.scala
+++ main/scala/com/awesomesauce/minecraft/forge/awesometech/tile/machine/TileEntityMachineController.scala
@@ -1,18 +1,16 @@
 package com.awesomesauce.minecraft.forge.awesometech.tile.machine
 
 import cofh.api.energy.{EnergyStorage, IEnergyHandler}
-import com.awesomesauce.minecraft.forge.awesometech.api.{TMachineComponent, TMachine}
-import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.inventory.IInventory
+import com.awesomesauce.minecraft.forge.awesometech.api.{TMachine, TMachineComponent}
+import com.awesomesauce.minecraft.forge.core.lib.MultiFluidTank
 import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
 import net.minecraftforge.common.util.ForgeDirection
-import net.minecraftforge.fluids.{FluidTank, FluidStack}
-import net.minecraftforge.oredict.OreDictionary
+import net.minecraftforge.fluids.FluidStack
 
 class TileEntityMachineController extends TileEntity with TMachine with IEnergyHandler {
   var storage: EnergyStorage = new EnergyStorage(100000)
-  var machineTanks : Array[FluidTank] = Array[FluidTank](new FluidTank(3000), new FluidTank(3000), new FluidTank(3000), new FluidTank(3000), new FluidTank(3000))
+  var machineTanks = new MultiFluidTank(5, 3000)
   var checkCounter = 400
   var xLength: Int = 0
   var yLength: Int = 0
@@ -44,10 +42,12 @@ class TileEntityMachineController extends TileEntity with TMachine with IEnergyH
   def getX = xCoord
   def getY = yCoord
   def getZ = zCoord
-  def drainFluid(fluid: FluidStack, simulate: Boolean): Boolean = false
+
+  def drainFluid(fluid: FluidStack, simulate: Boolean): FluidStack = machineTanks.drainFluid(fluid, simulate)
   def drainItem(item: ItemStack, simulate:Boolean): Boolean = false
   def drainRF(amount:Int, simulate: Boolean): Int = storage.extractEnergy(amount, simulate)
-  def insertFluid(fluid: FluidStack, simulate: Boolean): Boolean = false
+
+  def insertFluid(fluid: FluidStack, simulate: Boolean): FluidStack = machineTanks.insertFluid(fluid, simulate)
   def insertItem(item: ItemStack, simulate: Boolean): Boolean = false
   def insertRF(amount:Int, simulate: Boolean): Int = storage.receiveEnergy(amount, simulate)
   /*
