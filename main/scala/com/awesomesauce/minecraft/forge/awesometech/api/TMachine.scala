09539e6d71a563fb0f2491f26cb52f1432dcae70
gjgfuj <gjgfujb@gmail.com>
2014-08-17 16:21:06 +1000

    Added fluid stuff.

--- main/scala/com/awesomesauce/minecraft/forge/awesometech/api/TMachine.scala
+++ main/scala/com/awesomesauce/minecraft/forge/awesometech/api/TMachine.scala
@@ -5,13 +5,14 @@ import net.minecraftforge.fluids.FluidStack
 
 trait TMachine {
   def drainRF(amount: Int, simulate: Boolean) : Int
-
   def drainFluid(fluid: FluidStack, simulate: Boolean): FluidStack
-  def drainItem(item: ItemStack, simulate:Boolean) : Boolean
-  def insertRF(amount: Int, simulate: Boolean) : Int
 
+  def drainItem(item: ItemStack, simulate: Boolean): ItemStack
+
+  def insertRF(amount: Int, simulate: Boolean): Int
   def insertFluid(fluid: FluidStack, simulate: Boolean): FluidStack
-  def insertItem(item: ItemStack, simulate: Boolean) : Boolean
+
+  def insertItem(item: ItemStack, simulate: Boolean): ItemStack
   def getX: Int
   def getY: Int
   def getZ: Int
